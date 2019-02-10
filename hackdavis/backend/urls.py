e4fb205bea8642fdd008ef90aa7bb9aadfc9353e
CubeOfCheese <cubeofcheese@gmail.com>
2019-02-10 10:29:07 -0800

    fixed navigation bar

--- hackdavis/backend/urls.py
+++ hackdavis/backend/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns=[
 
     path("", views.index, name="index"),
+    path("index", views.index, name="index"),
     path("account", views.account, name="account"),
     path("event", views.event, name="event"),
     path("map", views.map, name="map"),
