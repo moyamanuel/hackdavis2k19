dafb8435aa94adb4663303d7b59250864aad85c7
CubeOfCheese <cubeofcheese@gmail.com>
2019-02-10 10:15:53 -0800

    fixed navbar on index.html

--- hackdavis/backend/urls.py
+++ hackdavis/backend/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path("", views.index, name="index")
+    
+    path("", views.index, name="index"),
+    path("account", views.account, name="account"),
+    path("event", views.event, name="event"),
+    path("map", views.map, name="map"),
 ]
--- hackdavis/backend/views.py
+++ hackdavis/backend/views.py
@@ -4,3 +4,12 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, "backend/index.html")
+
+def account(request):
+    return render(request, "backend/account.html")
+
+def map(request):
+    return render(request, "backend/map.html")
+
+def event(request):
+    return render(request, "backend/events.html")
