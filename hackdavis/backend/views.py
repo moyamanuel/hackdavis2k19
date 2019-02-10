192f6ac4662a0ee7a9ad4649c2cf4340de498e37
Manuel Moya <moya.manuel41@gmail.com>
2019-02-10 10:12:31 -0800

    Fix file structure

--- hackdavis/backend/views.py
+++ hackdavis/backend/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from backend.models import Event
 
 # Create your views here.
 def index(request):
+    if request.method == "POST":
+        new_event = Event(
+            image_link= request.POST.get('image_link'),
+            event_address= request.POST.get('event_address'),
+            importance_rank= request.POST.get('importance_name'),
+            event_description=request.POST.get('event_description')
+        )
+        new_event.save()
     return render(request, "backend/index.html")
+
+def create(request):
+    return render(request, "backend/account.html")
