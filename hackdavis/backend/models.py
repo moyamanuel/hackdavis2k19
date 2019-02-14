e800e806b41421bade0413d469e0823fe1fa6035
Manuel Moya <moya.manuel41@gmail.com>
2019-02-14 10:22:51 -0800

    Add Event model

--- hackdavis/backend/models.py
+++ hackdavis/backend/models.py
@@ -2,7 +2,4 @@ from django.db import models
 
 # Create your models here.
 class Event(models.Model):
-    image_link = models.CharField(max_length=150)
-    event_address = models.CharField(max_length=300)
-    importance_rank = models.IntegerField()
-    event_description = models.CharField(max_length=300)
+    event_description = models.CharField(max_length=50)
