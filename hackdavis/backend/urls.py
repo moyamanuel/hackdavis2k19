dfb895cf1dc93902ba7b54760235637769d6acca
Manuel Moya <moya.manuel41@gmail.com>
2019-02-14 10:23:07 -0800

    Add account route

--- hackdavis/backend/urls.py
+++ hackdavis/backend/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns=[
     path("", views.index, name="index"),
     path("index", views.index, name="index"),
-    path("create", views.create_user, name="create"),
+    path("account", views.create_user, name="create"),
     path("event", views.event, name="event"),
     path("map", views.map, name="map"),
 ]
